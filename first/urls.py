f3b18a62c4f933abe24ddd90d6547cbe8604d73c
Hasan Shah <hasanshah5882@gmail.com>
2018-12-03 21:56:24 +0400

    using the blogs webapp as main app

--- first/urls.py
+++ first/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('blogs/', include('blogs.urls')),
+    path('', include('blogs.urls')),
     path('admin/', admin.site.urls),
 ]
