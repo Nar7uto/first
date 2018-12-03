ff5bbe9d7c1d95194dc7acb9c5318a407e43eff3
Hasan Shah <hasanshah5882@gmail.com>
2018-12-04 02:06:04 +0400

    just added my first model

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+Class SignUp(models.Model):
+	email = models.EmailField()
+	fullname = models.CharField()
+	timestamp = models.DateTimeField()
+	updated = models.DateTimeField()
+
+	def __str__(self):
+		return self.email
+
