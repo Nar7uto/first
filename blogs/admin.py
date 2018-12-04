aab4f48734fa5462b4d81262c0213943f9eb0f2e
Hasan Shah <hasanshah5882@gmail.com>
2018-12-04 17:47:08 +0400

    edited the form in admin, for more learn about ModelAdmin in Django Doc

--- blogs/admin.py
+++ blogs/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Post, SignUp
 # Register your models here.
 
+class SignUpAdmin(admin.ModelAdmin):
+	list_display = ['__str__','timestamp','updated']
+	class Meta:
+		model = SignUp
 
-admin.site.register(SignUp)
+
+
+admin.site.register(SignUp, SignUpAdmin)
 admin.site.register(Post)
